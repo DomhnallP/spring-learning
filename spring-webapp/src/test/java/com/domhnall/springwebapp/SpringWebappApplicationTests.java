b8f9d087691c41cbaec12f7f5f5361ec773318a9
Domhnall ó Póil <dopoil@allfinanz.com>
2019-11-26 14:52:07 +0000

    Initial Commit, Created basing JPA objects

---
+++ spring-webapp/src/test/java/com/domhnall/springwebapp/SpringWebappApplicationTests.java
@@ -0,0 +1,12 @@
+package com.domhnall.springwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
